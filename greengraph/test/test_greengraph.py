358eca9045ccab5f55d9436709b5186fd1d8b8f2
David Twomey <david.twomey@live.com>
2016-01-06 19:52:32 +0000

    Added test of greengraph location_sequence method

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -1,9 +1,9 @@
 import yaml
 import os
-
+import numpy as np
 
 from ..greengrapher import Greengraph
-from nose.tools import assert_raises, assert_equal
+from nose.tools import assert_raises, assert_equal, assert_almost_equal
 
 
 
@@ -14,7 +14,7 @@ def test_Greengraph():
 	assert_equal(mygraph.start, 'London')
 	assert_equal(mygraph.end, 'Oxford')
 	
-# Test Greengraph class method geolocate
+# Test Greengraph(object) class method geolocate
 def test_geolocate_valid():
 	with open(os.path.dirname(__file__),'fixtures','sample_greengraph.yaml')) as fixture_file:
 		fixtures =yaml.load(fixture_file)['geolocate']
@@ -25,4 +25,18 @@ def test_geolocate_valid():
 		answer = (ans_lat, ans_long)
 		geo = Greengraph(0.0, 0.0)
 		assert_equal(geo.geolocate(location), answer)
+
+# Test Greengraph(object) class method location_sequence
+def test_location_sequence():
+	with open(os.path.join(os.path.dirname(__file__),'fixtures','sample_greengraph.yaml')) as fixture_file:
+		fixtures = yaml.load(fixture_file)['location_sequence']
+	for fix in fixtures:
+		start = fix.pop('start')
+		end = fix.pop('end')
+		steps = fix.pop('steps')
+		answer = fix.pop('answer')
+		geo = Greengraph(0.0, 0.0)
+		return_val = geo.location_sequence(geo.geolocate(start), geo.geolocate(end), steps)
+		answer = np.vstack(answer)
+		assert(np.array_equal(return_val, answer))
 		
