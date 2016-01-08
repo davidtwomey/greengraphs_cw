2a44bc01adff8806bf9c023936c2a189506cd527
David Twomey <david.twomey@live.com>
2016-01-08 18:30:36 +0000

    corrected import filename error and moved parser outside func

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,18 +1,18 @@
 from matplotlib import pyplot as plt
-from greengrapher import Greengraph
+from greengraph import Greengraph
 from argparse import ArgumentParser
 
 
+parser = ArgumentParser(description = "Evaluate green pixels between two locations")
+	
+parser.add_argument('--from', '-f', help = 'Start location', dest='startLoc')
+parser.add_argument('--to','-t', help = 'End location', dest='endLoc')
+parser.add_argument('--steps','-s', type= int, help = 'How many steps between locations to evaluate',default = 20)
+parser.add_argument('--out','-o',help= 'Define output filename. Default: PNG',default = 'output.png', dest='out')
+
 
 def process():
-	parser = ArgumentParser(description = "Evaluate green pixels between two locations")
-	
-	parser.add_argument('--from', '-f', help = 'Start location', dest='startLoc')
-	parser.add_argument('--to','-t', help = 'End location', dest='endLoc')
-	parser.add_argument('--steps','-s', type= int, help = 'How many steps between locations to evaluate',default = 20)
-	parser.add_argument('--out','-o',help= 'Define output filename. Default: PNG',default = 'output.png')
 	arguments= parser.parse_args()
-	
 	graph = Greengraph(arguments.startLoc,arguments.endLoc)
 	data = graph.green_between(arguments.steps)
 	
