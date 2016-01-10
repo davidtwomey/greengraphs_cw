92b519b4099c1210c1891b2df0494ce07edf2eb2
David Twomey <david.twomey@live.com>
2016-01-10 14:18:42 +0000

    restructured command.py for easier testing

--- greengraph/command.py
+++ greengraph/command.py
@@ -10,21 +10,23 @@ parser.add_argument('--to','-t', help = 'End location, default: Paris', dest='en
 parser.add_argument('--steps','-s', type= int, help = 'How many steps between locations to evaluate, default:20',default = 20,dest='steps')
 parser.add_argument('--out','-o',help= 'Define output filename. Type: PNG',default = 'output', dest='out')
 
-
-
-def process():
-    arguments= parser.parse_args()
-    graph = Greengraph(arguments.startLoc,arguments.endLoc)
-    data = graph.green_between(arguments.steps)
+def build_greengraph(args):
+    graph = Greengraph(args.startLoc,args.endLoc)
+    data = graph.green_between(args.steps)
     plt.plot(data)
     
     # Labelling of plot 
     plt.xlabel("Step")
     plt.ylabel("Number of green pixels (Max 160000)")
     plt.title("Green pixels in geographical images between "+
-              "".join(arguments.startLoc)+ " and " + "".join(arguments.endLoc))
-    plt.savefig(arguments.out+'.png')
+              "".join(args.startLoc)+ " and " + "".join(args.endLoc))
+    plt.savefig(args.out+'.png')
     plt.show()
+
+def process():
+    args= parser.parse_args()
+    build_greengraph(args)
+    
 		
 if __name__ == "__main__":
 	process()
