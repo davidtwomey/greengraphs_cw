b680aa587a9931b3d1d1ed6c63d4f700153af71c
David Twomey <david.twomey@live.com>
2016-01-10 23:58:42 +0000

    corrected typos in setup file

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Greengraphs",
-        author = "David Twomey"
-        author_email = "david.twomey.15@ucl.ac.uk"
+        author = "David Twomey",
+        author_email = "david.twomey.15@ucl.ac.uk",
     version = "1.0",
-        license = "MIT"
-        description = "Package which generates a graph of the proportion of green pixels in a series of satellite images between two points"
+        license = "MIT",
+        description = "Package which generates a graph of the proportion ofgreen pixels in a series of satellite images between two points",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greengraph'],
     install_requires = ['argparse', 'geopy', 'numpy', 'requests', 'matplotlib', 'mock']
