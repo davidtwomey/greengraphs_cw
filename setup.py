bc266ba65c1d7cc0aeb03fff8580c893e3bb44ba
David Twomey <david.twomey@live.com>
2016-01-10 21:51:34 +0000

    improved setup.py details

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Greengraphs",
-    version = "0.1",
+        author = "David Twomey"
+        author_email = "david.twomey.15@ucl.ac.uk"
+    version = "1.0",
+        license = "MIT"
+        description = "Package which generates a graph of the proportion of green pixels in a series of satellite images between two points"
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greengraph'],
     install_requires = ['argparse', 'geopy', 'numpy', 'requests', 'matplotlib', 'mock']
