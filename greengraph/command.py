28a163b252278f38b9505c16d707146bf30fee7a
David Twomey <david.twomey@live.com>
2016-01-10 14:08:05 +0000

    Added plot labels in command.py

--- greengraph/command.py
+++ greengraph/command.py
@@ -5,9 +5,9 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description = "Evaluate green pixels between two locations")
 	
-parser.add_argument('--from', '-f', help = 'Start location', dest='startLoc')
-parser.add_argument('--to','-t', help = 'End location', dest='endLoc')
-parser.add_argument('--steps','-s', type= int, help = 'How many steps between locations to evaluate',default = 20)
+parser.add_argument('--from', '-f', help = 'Start location, default: London', dest='startLoc', default='London')
+parser.add_argument('--to','-t', help = 'End location, default: Paris', dest='endLoc',default = 'Paris')
+parser.add_argument('--steps','-s', type= int, help = 'How many steps between locations to evaluate, default:20',default = 20,dest='steps')
 parser.add_argument('--out','-o',help= 'Define output filename. Type: PNG',default = 'output', dest='out')
 
 
@@ -16,8 +16,13 @@ def process():
     arguments= parser.parse_args()
     graph = Greengraph(arguments.startLoc,arguments.endLoc)
     data = graph.green_between(arguments.steps)
-	
     plt.plot(data)
+    
+    # Labelling of plot 
+    plt.xlabel("Step")
+    plt.ylabel("Number of green pixels (Max 160000)")
+    plt.title("Graph of green pixels in geographical images between"+
+              " ".join(arguments.startLoc)+ "and" + " ".join(arguments.endLoc))
     plt.savefig(arguments.out+'.png')
     plt.show()
 		
